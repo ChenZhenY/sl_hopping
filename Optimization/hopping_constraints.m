adc08a3768fe50591000aa0eb81c4cb0bb125eb1
vkchen <vkchen@mit.edu>
2021-11-16 21:43:24 -0500

    Added in constraints for touchdown and liftoff angles; optimize for velocity vector at takeoff

--- Optimization/hopping_constraints.m
+++ Optimization/hopping_constraints.m
@@ -23,7 +23,7 @@ function [cineq ceq] = hopping_constraints(x,z0,p)
 % todo: add slip constraints
 
     tf = p(end);
-    ctrl = x;
+    ctrl = x; % todo: change
     ctrl = vertcat(x, zeros(1,size(ctrl,2)));
     [tout, zout, uout, indices, slip_out] = hybrid_simulation(z0, ctrl, p, [0 tf],1);
     theta1 = zout(1,:); 
@@ -31,6 +31,16 @@ function [cineq ceq] = hopping_constraints(x,z0,p)
     COM = COM_jumping_leg(zout,p);
     ground_height = p(end-1);
     pos_leg = position_foot(zout(:,end),p);
+    
+    pend_vector_begin = position_foot(zout(:,1),p) - position_mount(zout(:,1),p);
+    pend_vector_end = position_foot(zout(:,end),p) - position_mount(zout(:,end),p);
+    
+    % find angles to the ground
+    u = [1; 0; 0];
+    cos_theta = max(min(dot(u,pend_vector_begin)/(norm(u)*norm(pend_vector_begin)),1),-1);
+    touchdown_angle = real(acos(cos_theta));
+    cos_theta = max(min(dot(u,pend_vector_begin)/(norm(u)*norm(pend_vector_begin)),1),-1);
+    liftoff_angle = real(acos(cos_theta));
     
     % check if anything other than the hopping foot touches the ground
 
@@ -46,13 +56,14 @@ function [cineq ceq] = hopping_constraints(x,z0,p)
     
     cineq = [...
         -theta2(:) + pi/6;... % joint limits
-%         theta1(:) + theta2(:)-2*pi/3;...
+        theta1(:) + theta2(:)-2*pi/3;...
 %         theta2(:) - 2*pi/3;...
         -theta1(:) - pi/3;...
 %         -sw_Cy(:); ...            % swinging leg does not contact floor
 %         -k_Cy(:); ...             % knee does not contact floor
 %         -h_Cy(:);...              % hip does not contact floor
 %         slip_out(:);...           % foot does not slip
+        touchdown_angle + liftoff_angle - pi; ...
         -(zout(4,end) - zout(4,1) - .1) ]; % leg moves forward in x direction
     
     % ceq = [min(zout(3,:)) - max(zout(3,:))]; % swing leg angle stays fixed
