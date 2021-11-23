066e7ad91f88c059394a4b0fecb0508b90afc32f
vkchen <vkchen@mit.edu>
2021-11-22 23:05:42 -0500

    Bugfix: stance phase and flight phase simulations working!

--- Modeling/test_joint_traj.m
+++ Modeling/test_joint_traj.m
@@ -1,7 +1,7 @@
 t  = 60;
 
-[th1, th2] = initial_condition_convert(2*pi/3, 0.18);
-mid_l = 0.05;
+[th1, th2] = initial_condition_convert(pi/3, 0.18);
+mid_l = 0.12;
 thd_1_array = 0;
 thd_2_array = 0;
 foot = zeros(3,60);
@@ -20,3 +20,5 @@ t = linspace(0,0.6,60);
 % plot(t,thd_1_array); % orange
 
 plot(foot(1,:), foot(2,:)); %yellow
+hold on;
+plot(t, foot(1,:));
